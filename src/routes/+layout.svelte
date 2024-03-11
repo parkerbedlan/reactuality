765f90337544f2ff127af8c74069feae81a9c5b3
Parker Bedlan <parkerbedlan@gmail.com>
2024-03-11 15:41:59 -0400

    added merch iframe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,32 @@
-<script>import "../app.postcss";</script>
+<script>
+    import NavLink from "./NavLink.svelte";
+import "../app.postcss";</script>
 <svelte:head>
     <title>RE: ACTUALITY</title>
 </svelte:head>
+<header class="border-b flex items-center justify-center">
+    <a href="/" class="group">
+        <img
+            src="/episode-1-sole-title.jpg"
+            alt="Re:Actuality - Rules of the Universe"
+            class="h-24 group-active:scale-50 duration-75"
+        />
+    </a>
+</header>
+<nav
+    class="border-b sticky flex items-center justify-center sm:gap-8 font-r11y h-8 mb-4 top-0 bg-background [&_*]:!flex-nowrap"
+>
+    <div class="border-r px-1 sm:border-none">
+        <NavLink text="Pilot" href="/#pilot" />
+    </div>
+    <div class="text-6xl hidden sm:block">-</div>
+    <div class="border-r px-1 sm:border-none">
+        <NavLink text="Merch" href="/merch" />
+    </div>
+    <div class="text-6xl hidden sm:block">-</div>
+    <div class="px-1 sm:border-none">
+        <NavLink text="Links" href="/#links" />
+    </div>
+</nav>
+
 <slot></slot>
---
+++ src/routes/merch/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import RainbowText from "../RainbowText.svelte";
+</script>
+
+<div
+    class="text-4xl text-center flex flex-wrap justify-center -z-10 [&_*]:!flex-nowrap w-full absolute"
+>
+    <RainbowText text="opening" />
+    <RainbowText text=" closet..." />
+</div>
+<iframe
+    title="Merch Shop"
+    src="https://reactuality.myspreadshop.com/"
+    class="w-full h-[300vh] select-none overflow-y-visible z-10"
+></iframe>
